27373e7d52a77ed2128abab6e298c47108954997
TalPiven13 <157593149+TalPiven13@users.noreply.github.com>
2024-04-14 17:42:47 +0300

    Update TonConnect manifest with new URL and name

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,14 +2,13 @@
 // import { dirname, resolve } from 'node:path';
 // import { fileURLToPath } from 'node:url';
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
